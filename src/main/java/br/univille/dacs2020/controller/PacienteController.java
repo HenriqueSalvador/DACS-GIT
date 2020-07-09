5f8cd929c6d4aea0514ece0882524455ee0dba88
HENRIQUE SALVADOR <henrique.salvador@univille.br>
2020-07-09 23:29:08 +0000

    Comiitersssom

--- src/main/java/br/univille/dacs2020/controller/PacienteController.java
+++ src/main/java/br/univille/dacs2020/controller/PacienteController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -24,5 +27,22 @@ public class PacienteController {
     public ModelAndView index(){
         List<Paciente> listaPacientes = service.getaLL();
         return new ModelAndView("paciente/index","listapacientes",listaPacientes);
+    }
+
+    @GetMapping("/novo")
+    public ModelAndView createForm(@ModelAttribute Paciente paciente) {
+        return new ModelAndView("paciente/form");
+    }
+
+    @PostMapping(params="form")
+    public ModelAndView save(Paciente paciente){
+        service.save(paciente);;
+        
+        return new ModelAndView("redirect:/paciente");
+    }
+
+    @GetMapping(value="/alterar/{id}")
+    public ModelAndView edit(@PathVariable("id") long id, Paciente paciente) {
+        return new ModelAndView("paciente/form","paciente",paciente);
     }
 }
