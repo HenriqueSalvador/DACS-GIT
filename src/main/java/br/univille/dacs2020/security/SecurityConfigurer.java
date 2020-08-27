f83286ef90e2d5d3492ad44212d429da687ce72b
HENRIQUE SALVADOR <henrique.salvador@univille.br>
2020-08-27 22:58:33 +0000

    commit

--- src/main/java/br/univille/dacs2020/security/SecurityConfigurer.java
+++ src/main/java/br/univille/dacs2020/security/SecurityConfigurer.java
@@ -2,19 +2,25 @@ package br.univille.dacs2020.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
- 
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import br.univille.dacs2020.service.impl.MyUserDetailsService;
  
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyUserDetailsService myUserDetailsService;
- 
+    @Autowired
+    private JWTRequestFilter jwtRequestFilter;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService);
@@ -23,5 +29,25 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+    @Override
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        .authorizeRequests().antMatchers("/api/v1/auth/signin").permitAll()
+        .antMatchers("/api/**").authenticated()
+        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+     
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+
+        // TODO Auto-generated method stub
+
+        return super.authenticationManager();
+
     }
 }
