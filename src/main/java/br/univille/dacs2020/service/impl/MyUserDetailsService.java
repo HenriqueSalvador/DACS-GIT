268a375e53984ea8c68599618997b1fa56d0732d
HENRIQUE SALVADOR <henrique.salvador@univille.br>
2020-09-03 23:17:46 +0000

    commit

--- src/main/java/br/univille/dacs2020/service/impl/MyUserDetailsService.java
+++ src/main/java/br/univille/dacs2020/service/impl/MyUserDetailsService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
  
 import br.univille.dacs2020.repository.UsuarioRepository;
@@ -15,7 +16,13 @@ public class MyUserDetailsService implements UserDetailsService {
  
     @Autowired
     private UsuarioRepository service; 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
  
+    public Usuario buscaUsuario(String nomeUsuario){        
+        return service.findByUsuario(nomeUsuario);    
+    }
+
     public Usuario buscaUsuarioSenha(String nomeUsuario, String senhaUsuario){
         return service.findByUsuarioAndSenha(nomeUsuario, senhaUsuario);
     }
@@ -26,4 +33,8 @@ public class MyUserDetailsService implements UserDetailsService {
         return new User(usuario.getUsuario(),usuario.getSenha(), new ArrayList<>());
     }  
      
+    public void save(Usuario usuario){
+        if (usuario.getSenha().length() != 0)               
+                usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+          service.save(usuario);    }
 }
